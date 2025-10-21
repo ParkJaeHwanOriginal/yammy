4b71bd7fb5f2a9ec830be111ab90f6e8457bf64e
박재환 <pogpog1230@naver.com>
2025-10-21 12:04:34 +0900

    야미 백엔드 교체

--- yammy/src/main/java/com/yammy/yammy/YammyApplication.java
+++ yammy/src/main/java/com/yammy/yammy/YammyApplication.java
@@ -2,8 +2,10 @@ package com.yammy.yammy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Spring Security 자동 설정을 비활성화하기 위해 임포트합니다.
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // 이 부분을 수정하세요.
 public class YammyApplication {
 
 	public static void main(String[] args) {
