c3705aef43003599b4b1c762207932298c4cd9c7
박재환 <pogpog1230@naver.com>
2025-10-22 16:23:39 +0900

    이건 로그인 요구하는거

--- yammy/src/main/java/com/yammy/yammy/YammyApplication.java
+++ yammy/src/main/java/com/yammy/yammy/YammyApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // <--- 이 부분을 추가합니다.
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // <--- 이 부분을 추가합니다.
+@SpringBootApplication() // <--- 이 부분을 추가합니다.
 public class YammyApplication {
 
 	public static void main(String[] args) {
